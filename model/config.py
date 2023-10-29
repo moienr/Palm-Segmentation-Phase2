3c9f3de62359a6be36efdcce720c3b3d121aec8f
moi3n <moi3nr@gmail.com>
2023-10-30 01:24:17 +0330

    minor

--- model/config.py
+++ model/config.py
@@ -1,6 +1,5 @@
 import torch
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
+
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 TRAIN_DIR = "data/train"
@@ -18,12 +17,4 @@ CHECKPOINT_GEN_Z = "genz.pth.tar"
 CHECKPOINT_CRITIC_H = "critich.pth.tar"
 CHECKPOINT_CRITIC_Z = "criticz.pth.tar"
 
-transforms = A.Compose(
-    [
-        A.Resize(width=256, height=256),
-        A.HorizontalFlip(p=0.5),
-        A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255),
-        ToTensorV2(),
-    ],
-    additional_targets={"image0": "image"},
-)
+
